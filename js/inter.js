14549d261c17654e6a8b1a6adae7217416a64216
wokan-mike <butao_jck_imt@hotmail.com>
2018-11-26 12:48:06 -0600

    wedwed

--- js/inter.js
+++ js/inter.js
@@ -37,44 +37,32 @@ interact('.resize-drag')
         'rotate(' + angle + 'deg)';
     }
   })
-  .resizable({
-      squareResize: true,
-      // resize from all edges and corners
-      edges: { left: true, right: true, bottom: true, top: true },
+  interact('.resize-drag2')
+  .draggable({
+    // enable inertial throwing
+    // inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+    // restriction: "parent",
+    endOnly: true,
+    elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
 
-      // keep the edges inside the parent
-      restrictEdges: {
-      outer: 'parent',
-      endOnly: true,
-      },
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+    var textEl = event.target.querySelector('p');
 
-      // minimum size
-      restrictSize: {
-      min: { width: 50, height: "auto" },
-      },
-
-      inertia: true,
-  })
-  .on('resizemove', function (event) {
-      var target = event.target,
-          x = (parseFloat(target.getAttribute('data-x')) || 0),
-          y = (parseFloat(target.getAttribute('data-y')) || 0);
-
-      // update the element's style
-      target.style.width  = event.rect.width + 'px';
-      target.style.height = event.rect.width + 'px';
-
-      // translate when resizing from top or left edges
-      x += event.deltaRect.left;
-      y += event.deltaRect.top;
-
-      target.style.webkitTransform = target.style.transform =
-          'translate(' + x + 'px,' + y + 'px)';
-
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
-      target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
-  });
+    textEl && (textEl.textContent =
+        'moved a distance of '
+        + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                    Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px');
+    }
+})
   function dragMoveListener (event) {
       var target = event.target,
           // keep the dragged position in the data-x/data-y attributes
